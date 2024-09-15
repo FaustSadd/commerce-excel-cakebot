496b04b142219865d178e189b0298b139061fa87
FaustSadd <shulgaegor06@gmail.com>
2024-09-15 18:31:11 +0300

    Подключен TG Api, создана структура проекта

---
+++ README.md
@@ -0,0 +1,2 @@
+# commerce-excel-cakebot
+Readme
---
+++ src/test/java/Omg/CakeBot/CakeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package Omg.CakeBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
