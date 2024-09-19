9511d7ba44729aaed961b1cfe4943d7b8ddf5245
FaustSadd <shulgaegor06@gmail.com>
2024-09-19 16:37:07 +0300

    Исправлены баги. Оптимизирована внутренняя логика бота. Удалён класс GoogleSheetsCore. Функции этого класса перенсены в LogicCore и интегрированы с ним, в целях оптимизации, предотвращения ошибок при работе с Google Sheets API, предотвращения дублирования кода. Добавлены несколько команд при работе с ботом. Создан метод для правильной работы с google таблицей, поиска пустых строк.

--- src/main/java/Omg/CakeBot/CakeBotApplication.java
+++ src/main/java/Omg/CakeBot/CakeBotApplication.java
@@ -3,12 +3,13 @@ package Omg.CakeBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 @SpringBootApplication
 public class CakeBotApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws GeneralSecurityException, IOException {
 		SpringApplication.run(CakeBotApplication.class, args);
 	}
-
-
 }
--- src/main/java/Omg/CakeBot/Service/LogicCore.java
+++ src/main/java/Omg/CakeBot/Service/LogicCore.java
@@ -3,15 +3,23 @@ package Omg.CakeBot.Service;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 
 public class LogicCore {
     private String name;
@@ -20,14 +28,42 @@ public class LogicCore {
     private float weight;
     private String address;
     private String phoneNumber;
-    GoogleSheetsCore googleSheetsCore = new GoogleSheetsCore();  // Экземпляр класса для работы с Google Sheets
+    private int rowIndex = 2;  // Изначально предполагаем, что свободная строка найдется не ниже 2-й
 
-
+    private static final String APPLICATION_NAME = "Google Sheets API Java";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
     private static final String SPREADSHEET_ID = "1YqZzWReUmmlqtpyBY50tCr8bh4Mckye1rYTZTJeMutY";
-    private static final String RANGE = "Sheet1!A1:F1";  // Укажите реальный диапазон ячеек в таблице
 
+    private Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/excelbot-435816-753e1ba5d64a.json"))
+                .createScoped(Collections.singletonList("https://www.googleapis.com/auth/spreadsheets"));
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpCredentialsAdapter(credentials))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
     public String getStandartReceipt(String messageText) {
+        // Парсим данные из сообщения
+        parseOrderDetails(messageText);
+
+        String responseMessage = "Имя покупателя: " + name + "\n" +
+                "Дата заказа: " + date + "\n" +
+                "Название продукта: " + productName + "\n" +
+                "Вес: " + weight + "\n" +
+                "Адрес: " + address + "\n" +
+                "Номер телефона: " + phoneNumber;
+
+        try {
+            findAvailableRow();  // Найти первую свободную строку перед записью
+            writeToGoogleSheets();  // Записать данные в Google Sheets
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return responseMessage;
+    }
+
+    private void parseOrderDetails(String messageText) {
         // Парсим имя
         Pattern namePattern = Pattern.compile("Получен заказ от покупателя (.+) :");
         Matcher nameMatcher = namePattern.matcher(messageText);
@@ -57,7 +93,6 @@ public class LogicCore {
         if (addressMatcher.find()) {
             address = addressMatcher.group(2).trim();
         } else {
-            // Парсим самовывоз
             Pattern pickupPattern = Pattern.compile("(Самовывоз.+)");
             Matcher pickupMatcher = pickupPattern.matcher(messageText);
             if (pickupMatcher.find()) {
@@ -71,46 +106,42 @@ public class LogicCore {
         if (phoneMatcher.find()) {
             phoneNumber = phoneMatcher.group(1);
         }
-
-        String responseMessage = "Имя покупателя: " + name + "\n" +
-                "Дата заказа: " + date + "\n" +
-                "Название продукта: " + productName + "\n" +
-                "Вес: " + weight + "\n" +
-                "Адрес: " + address + "\n" +
-                "Номер телефона: " + phoneNumber;
-
-        // Попробуем записать данные в Google Sheets через try catch, так как необходимо обработать исключение
-        try {
-            writeToGoogleSheets();  // Вызов метода записи в таблицу
-        } catch (Exception e) {
-            e.printStackTrace();  // Вывод ошибки в консоль
-        }
-        return responseMessage;
     }
 
-    // Метод для записи данных в Google Sheets
-    public void writeToGoogleSheets() throws Exception {
-        // Данные в одной строке
+    private void writeToGoogleSheets() throws Exception {
         List<List<Object>> values = Arrays.asList(
                 Arrays.asList(name, date.toString(), productName, weight, address, phoneNumber)
         );
 
-        System.out.println("Записываем данные: " + values);
-
-        // Получаем экземпляр Sheets через googleSheetsCore
-        Sheets service = googleSheetsCore.Sheets();
-
-        // Создаем объект ValueRange и передаем данные
+        Sheets service = getSheetsService();
+        String range = String.format("Sheet1!A%d:F%d", rowIndex, rowIndex);
         ValueRange body = new ValueRange().setValues(values);
-        System.out.println("Диапазон: " + RANGE + ", Spreadsheet ID: " + SPREADSHEET_ID);
 
-        // Обновляем таблицу Google Sheets
         UpdateValuesResponse result = service.spreadsheets().values()
-                .update(SPREADSHEET_ID, RANGE, body)
+                .update(SPREADSHEET_ID, range, body)
                 .setValueInputOption("RAW")
                 .execute();
 
-        // Выводим результат обновления
         System.out.println(result.getUpdatedCells() + " ячеек обновлено.");
+    }
+
+    private void findAvailableRow() throws IOException, GeneralSecurityException {
+        Sheets service = getSheetsService();
+        String rangeTemplate = "Sheet1!A%d:F%d";
+        int rowIndex = 2;  // начинаем с 2-й строки
+
+        while (true) {
+            String range = String.format(rangeTemplate, rowIndex, rowIndex);
+            ValueRange response = service.spreadsheets().values()
+                    .get(SPREADSHEET_ID, range)
+                    .execute();
+            List<List<Object>> values = response.getValues();
+
+            if (values == null || values.isEmpty() || values.get(0).isEmpty()) {
+                this.rowIndex = rowIndex;
+                break;  // Выход из цикла после нахождения первой пустой строки
+            }
+            rowIndex++;
+        }
     }
 }
